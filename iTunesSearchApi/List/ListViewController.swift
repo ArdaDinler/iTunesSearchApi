1e9a6a3f5498c13f57da69e5111f0c2acf09245d
Arda Dinler <adinler@ford.com.tr>
2022-09-27 08:06:49 +0300

    BugFixes for crash

--- iTunesSearchApi/List/ListViewController.swift
+++ iTunesSearchApi/List/ListViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ListViewController: UICollectionViewController {
+class ListViewController: UIViewController {
 
     // MARK: - Life Cycles
     override func viewDidLoad() {
