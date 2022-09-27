5f98385dfd872fa182bb5d58f848f403268a0e44
Arda Dinler <adinler@ford.com.tr>
2022-09-27 09:35:11 +0300

    Added diffable data sources for collection view and binding view model property of section

--- iTunesSearchApi/List/ListViewController.swift
+++ iTunesSearchApi/List/ListViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 import Combine
 
 class ListViewController: UICollectionViewController {
+    // MARK: - Value Types
+    typealias DataSource = UICollectionViewDiffableDataSource<Section, Picture>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Section, Picture>
 
     // MARK: - Properties
     private var bindings = Set<AnyCancellable>()
+    private lazy var dataSource = makeDataSource()
     private var searchController = UISearchController(searchResultsController: nil)
     var viewModel = ViewModel()
 
@@ -25,6 +29,8 @@ class ListViewController: UICollectionViewController {
     // MARK: - Private -
     private func prepareViewComponents() {
         configureSearchController()
+        configureLayout()
+        applySnapshot(animatingDifferences: false)
         bindSearchBar()
         bindViewModel()
     }
@@ -44,6 +50,48 @@ class ListViewController: UICollectionViewController {
     }
 
     private func bindViewModel() {
+        viewModel.$sections.receive(on: DispatchQueue.main).sink { [weak self] (items) in
+            guard let strongSelf = self,
+                  items != nil else { return }
+            strongSelf.applySnapshot(animatingDifferences: false)
+        }.store(in: &bindings)
+    }
+
+    private func makeDataSource() -> DataSource {
+        let dataSource = DataSource(
+            collectionView: collectionView,
+            cellProvider: { (collectionView, indexPath, picture) ->
+                UICollectionViewCell? in
+                let cell = collectionView.dequeueReusableCell(
+                    withReuseIdentifier: "PictureCollectionViewCell",
+                    for: indexPath) as? PictureCollectionViewCell
+                cell?.picture = picture
+                return cell
+            })
+        dataSource.supplementaryViewProvider = { collectionView, kind, indexPath in
+            guard kind == UICollectionView.elementKindSectionHeader else {
+                return nil
+            }
+            let section = self.dataSource.snapshot()
+                .sectionIdentifiers[indexPath.section]
+            let view = collectionView.dequeueReusableSupplementaryView(
+                ofKind: kind,
+                withReuseIdentifier: SectionHeaderReusableView.reuseIdentifier,
+                for: indexPath) as? SectionHeaderReusableView
+            view?.titleLabel.text = section.title
+            return view
+        }
+        return dataSource
+    }
+
+    private func applySnapshot(animatingDifferences: Bool = true) {
+        var snapshot = Snapshot()
+        snapshot.deleteAllItems()
+        snapshot.appendSections(viewModel.sections ?? [])
+        viewModel.sections?.forEach { section in
+            snapshot.appendItems(section.pictures, toSection: section)
+        }
+        dataSource.apply(snapshot, animatingDifferences: animatingDifferences)
     }
 
     private func configureSearchController() {
@@ -58,5 +106,38 @@ class ListViewController: UICollectionViewController {
 extension ListViewController: UISearchBarDelegate {
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         viewModel.searchQuery = ""
+    }
+}
+
+// MARK: - Layout Handling
+extension ListViewController {
+    private func configureLayout() {
+        collectionView.register(
+            SectionHeaderReusableView.self,
+            forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+            withReuseIdentifier: SectionHeaderReusableView.reuseIdentifier
+        )
+        collectionView.collectionViewLayout = createCompositionalLayout()
+    }
+
+    private func createCompositionalLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { (sectionNumber, env) -> NSCollectionLayoutSection? in
+            return self.createLayoutSection()
+        }
+    }
+
+    private func createLayoutSection() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.50),heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = .init(top: 0, leading: 0, bottom: 15, trailing: 15)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),heightDimension: .estimated(200))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = .init(top: 0, leading: 0, bottom: 15, trailing: 0)
+        section.contentInsets.leading = 15
+        section.orthogonalScrollingBehavior = .continuous
+        section.boundarySupplementaryItems = [
+            NSCollectionLayoutBoundarySupplementaryItem(layoutSize: .init(widthDimension:.fractionalWidth(1), heightDimension: .estimated(44)), elementKind: UICollectionView.elementKindSectionHeader, alignment:.topLeading)]
+        return section
     }
 }
