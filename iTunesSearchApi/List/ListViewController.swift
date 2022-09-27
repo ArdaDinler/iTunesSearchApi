41f40913977ed935f330f96aa8fae4156b1cdfde
Arda Dinler <adinler@ford.com.tr>
2022-09-27 09:28:20 +0300

    Adopt view model for searchig itnues screenshotUrls and getting images by using kingfiesher api. Also, images downloading photos by limiting gcd group and semaphore. Then added some helper method for calculation of image size

--- iTunesSearchApi/List/ListViewController.swift
+++ iTunesSearchApi/List/ListViewController.swift
@@ -6,12 +6,57 @@
 //
 
 import UIKit
+import Combine
 
 class ListViewController: UICollectionViewController {
-    
+
+    // MARK: - Properties
+    private var bindings = Set<AnyCancellable>()
+    private var searchController = UISearchController(searchResultsController: nil)
+    var viewModel = ViewModel()
+
     // MARK: - Life Cycles
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        prepareViewComponents()
+    }
+
+    // MARK: - Private -
+    private func prepareViewComponents() {
+        configureSearchController()
+        bindSearchBar()
+        bindViewModel()
+    }
+
+    private func bindSearchBar() {
+        NotificationCenter.default.publisher(for:
+                                                    UISearchTextField.textDidChangeNotification,
+                                                object: searchController.searchBar.searchTextField)
+            .map {
+                ($0.object as! UISearchTextField).text
+            }.debounce(for: .seconds(0.7), scheduler: DispatchQueue.main).removeDuplicates()
+            .sink { [weak self] (value) in
+                guard let strongSelf = self,
+                      let text = value else { return }
+                strongSelf.viewModel.searchQuery = text
+            }.store(in: &bindings)
+    }
+
+    private func bindViewModel() {
+    }
+
+    private func configureSearchController() {
+        searchController.searchBar.delegate = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+    }
+}
+
+extension ListViewController: UISearchBarDelegate {
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        viewModel.searchQuery = ""
     }
 }
