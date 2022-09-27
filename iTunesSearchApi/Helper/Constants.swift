ff7b849a323874cb1da50332a3a137baa8e936a1
Arda Dinler <adinler@ford.com.tr>
2022-09-27 08:08:42 +0300

    Added constant helper file for itunes base url

---
+++ iTunesSearchApi/Helper/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  iTunesSearchApi
+//
+//  Created by Arda Dinler on 27.09.2022.
+//
+
+import Foundation
+
+struct Constants {
+    struct iTunes {
+        static let baseURL = "https://itunes.apple.com/"
+    }
+}
