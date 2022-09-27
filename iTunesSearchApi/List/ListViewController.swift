21de157e5dead792eaccb45003935e768f15a7a4
Arda Dinler <adinler@ford.com.tr>
2022-09-27 08:04:10 +0300

    Added new view controller for listing screenshoturls by using collection view

---
+++ iTunesSearchApi/List/ListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ListViewController.swift
+//  iTunesSearchApi
+//
+//  Created by Arda Dinler on 27.09.2022.
+//
+
+import UIKit
+
+class ListViewController: UICollectionViewController {
+
+    // MARK: - Life Cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
